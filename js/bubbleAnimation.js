77814d62a9c39a79c79a098ed914b5aee7aa1119
Dana Istikharova <dana.istikharova@bluewin.ch>
2023-02-02 00:13:23 +0100

    animation

---
+++ js/bubbleAnimation.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.bubble',{
+    duration: 1500,
+    origin:'top',
+    distance:'10px',
+    reset: true,
+    easing: 'ease-in' 
+});
+ScrollReveal().reveal('.b-one',{
+    delay:500
+
+});
+ScrollReveal().reveal('.b-two',{
+    delay:200
+
+});
---
+++ js/textAnimation.js
@@ -0,0 +1,9 @@
+
+ScrollReveal().reveal('h1, h4, h2, h3, p',{
+    duration: 2000,
+    origin:'top',
+    distance:'10px',
+    viewFactor:1,
+    reset: true,
+    useDelay: 'once'
+});
