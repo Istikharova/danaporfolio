eb11b0665a8f40986b96042d825eae12de95a9eb
Dana Istikharova <dana.istikharova@bluewin.ch>
2023-02-04 12:57:20 +0100

    projekt

--- js/projects.js
+++ js/projects.js
@@ -1,25 +1,29 @@
-document.querySelector('.first').addEventListener("click", openProjectWe);
-
-function openProjectWe() {
-  document.querySelector('.Webdesign').style.display = "none";
-    document.querySelector('.graphicdesign').style.display = "none";
-    document.querySelector('.webentwicklung').style.display = "block";
-    
-  }
-
-document.querySelector('.two').addEventListener("click", openProjectWd);
+document.querySelector('.two').addEventListener("click", openProjectWd); 
+document.querySelector('.erste').addEventListener("click", openProjectWe);
+document.querySelector('.thre').addEventListener("click", openProjectGd); 
 
 function openProjectWd() {
     document.querySelector('.Webdesign').style.display = "block";
     document.querySelector('.webentwicklung').style.display = "none";
     document.querySelector('.graphicdesign').style.display = "none";
+    console.log("webdesign");
+    
   }
 
-document.querySelector('.thre').addEventListener("click", openProjectGd);
 
 function openProjectGd() {
     document.querySelector('.graphicdesign').style.display = "block";
     document.querySelector('.Webdesign').style.display = "none";
     document.querySelector('.webentwicklung').style.display = "none";
-  } 
+    console.log("graphicdesign");
+
+  }
+
+function openProjectWe() {
+  document.querySelector('.webentwicklung').style.display = "block";
+  document.querySelector('.graphicdesign').style.display = "none";
+  document.querySelector('.Webdesign').style.display = "none"; 
+  console.log("entwicklung");
+
+}
  
