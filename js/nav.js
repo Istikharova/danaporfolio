4f8197812521f35c4b806ba290ee9af24f8aea8c
Istikharova <dana.istikharova@bluewin.ch>
2024-11-17 23:04:27 +0100

    bruger menu

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,9 @@
-document.querySelector('.button-wrapper').addEventListener('click', function () {
-  document.querySelector('.nav-container').classList.toggle('active');
-});
+const burgerIcon = document.querySelector('.burger-icon');
+const menu = document.querySelector('.container-nav ul');
+
+burgerIcon.addEventListener('click', () => {
+  // Toggle the 'show' class to open and close the menu
+  menu.classList.toggle('show');
+  // Toggle the active state for the burger icon (optional for animation effects)
+  burgerIcon.classList.toggle('active');
+}); 
