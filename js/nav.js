d8bca4a5e3db5df817ee20ee8dd3da2fe7c37c44
Istikharova <dana.istikharova@bluewin.ch>
2024-12-02 20:38:18 +0100

    active  nav

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,19 @@
+/* nav link active */
+const navLinks = document.querySelectorAll('#main-nav ul li a');
+
+// Aktueller Pfad abrufen
+const currentPath = window.location.pathname;
+
+// Schleife durch die Links, um den aktiven Link zu markieren
+navLinks.forEach(link => {
+  // Den Pfad des Links extrahieren
+  const linkPath = new URL(link.href).pathname;
+  // Vergleich zwischen dem aktuellen Pfad und dem Link-Pfad
+  if (linkPath === currentPath) {
+    link.classList.add('link-active');
+  }
+});
+
 const burgerIcon = document.querySelector('.burger-icon');
 const menu = document.querySelector('.container-nav ul');
 
